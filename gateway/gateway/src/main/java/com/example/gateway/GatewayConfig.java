80f1bd168c4b4d787c5b94e009de048a1f52b2c6
Raunak <rounand44@gmail.com>
2026-05-29 23:55:02 +0530

    added configuration of gateway

--- gateway/gateway/src/main/java/com/example/gateway/GatewayConfig.java
+++ gateway/gateway/src/main/java/com/example/gateway/GatewayConfig.java
@@ -1,7 +1,24 @@
 package com.example.gateway;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayConfig {
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder)
+    {
+        return routeLocatorBuilder.routes()
+                .route("companyms",r->r.path("/api/company/**")
+                        .uri("lb://companyms"))
+                .route("jobms",r->r.path("/api/job/**")
+                        .uri("lb://jobms"))
+                .route("reviewms",r->r.path("/reviews/**")
+                        .uri("lb://reviewms"))
+                .build();
+
+    }
 }
