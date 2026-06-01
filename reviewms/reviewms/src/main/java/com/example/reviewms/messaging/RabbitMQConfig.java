52a63e6fcc4678107df0cf9d89543250fbe2ceb3
Raunak <rounand44@gmail.com>
2026-06-01 22:51:47 +0530

    added remaining config of rabbitmq

---
+++ reviewms/reviewms/src/main/java/com/example/reviewms/messaging/RabbitMQConfig.java
@@ -0,0 +1,79 @@
+package com.example.reviewms.messaging;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    @Value("${rabbitmq.queue.name}")
+    private String queueName;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    @Bean
+    public Queue queue()
+    {
+        return QueueBuilder.durable(queueName).build();
+
+    }
+
+    @Bean
+    public TopicExchange topicExchange()
+    {
+        return ExchangeBuilder
+                .topicExchange(exchangeName)
+                .durable(true)
+                .build();
+    }
+
+    @Bean
+    public Binding binding()
+    {
+        return BindingBuilder
+                .bind(queue())
+                .to(topicExchange())
+                .with(routingKey);
+    }
+
+    // this method automatically declares queues, binding and exchange when the app starts
+    @Bean
+    public AmqpAdmin amqpAdmin(ConnectionFactory connectionFactory)
+    {
+        RabbitAdmin rabbitAdmin=new RabbitAdmin(connectionFactory);
+        rabbitAdmin.setAutoStartup(true);
+
+        return rabbitAdmin;
+    }
+
+    // converts java objects into json and vice versa
+    @Bean
+    public MessageConverter messageConverter()
+    {
+        return new JacksonJsonMessageConverter();
+    }
+
+    // used to send messages to rabbitmq
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory)
+    {
+        RabbitTemplate rabbitTemplate=new RabbitTemplate(connectionFactory);
+
+        rabbitTemplate.setMessageConverter(messageConverter());
+
+        rabbitTemplate.setExchange(exchangeName);
+
+        return rabbitTemplate;
+    }
+}
