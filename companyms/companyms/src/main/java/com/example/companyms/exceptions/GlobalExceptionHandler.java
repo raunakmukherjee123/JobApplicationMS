ffa2c4a2cbd92c02b97e540422d833bc85d781cf
Raunak <rounand44@gmail.com>
2026-07-02 23:28:02 +0530

    handled error message for validations

--- companyms/companyms/src/main/java/com/example/companyms/exceptions/GlobalExceptionHandler.java
+++ companyms/companyms/src/main/java/com/example/companyms/exceptions/GlobalExceptionHandler.java
@@ -3,13 +3,15 @@ package com.example.companyms.exceptions;
 import com.example.companyms.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(CompanyNotFoundExceptions.class)
     public ResponseEntity<?> handleCompanyNotFoundException(CompanyNotFoundExceptions companyNotFoundExceptions)
@@ -22,6 +24,23 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+
+    //same as above, but we have to use @ControllerAdvice instead of @RestControllerAdvice
 
 //    @ExceptionHandler(CompanyNotFoundExceptions.class)
 //    @ResponseBody
