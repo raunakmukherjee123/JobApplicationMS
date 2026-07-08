c2fe6e94c4366a6676d91446201c7cf7a2acc2dc
Raunak <rounand44@gmail.com>
2026-07-08 22:38:16 +0530

    added global exception handler in reviewms

--- reviewms/reviewms/src/main/java/com/example/reviewms/exceptions/GlobalExceptionHandler.java
+++ reviewms/reviewms/src/main/java/com/example/reviewms/exceptions/GlobalExceptionHandler.java
@@ -1,11 +1,41 @@
 package com.example.reviewms.exceptions;
 
+import com.example.reviewms.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ReviewNotFoundException.class)
-    public
+    public ResponseEntity<?> handleReviewNotFoundException(ReviewNotFoundException reviewNotFoundException)
+    {
+        ErrorResponse errorResponse=new ErrorResponse();
+
+        errorResponse.setTime(LocalDateTime.now());
+        errorResponse.setMessage("No review found");
+        errorResponse.setDetails(reviewNotFoundException.getMessage());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 }
