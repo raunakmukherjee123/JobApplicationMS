7add22af9158bc6cfa5910e70392854052e8ecba
Raunak <rounand44@gmail.com>
2026-07-15 23:30:08 +0530

    corrected the query method

--- jobms/src/main/java/com/example/jobms/JobRepository.java
+++ jobms/src/main/java/com/example/jobms/JobRepository.java
@@ -6,7 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 public interface JobRepository extends JpaRepository<Job,Integer> {
 
     @Query("""
-            SELECT j.title, j.description, j.minSalary, j.maxSalary, j.location
+            SELECT j.title AS title,
+                   j.description AS description,
+                   j.minSalary AS minSalary,
+                   j.maxSalary AS maxSalary,
+                   j.location AS location
             FROM Job j
             WHERE j.id = :id
             """)
