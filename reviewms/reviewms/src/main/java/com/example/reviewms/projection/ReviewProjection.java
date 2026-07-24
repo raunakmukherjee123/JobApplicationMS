db5f1a5245a5d56299602525c67d53be484788cd
Raunak <rounand44@gmail.com>
2026-07-24 22:57:54 +0530

    added interface projection class of reviewms

---
+++ reviewms/reviewms/src/main/java/com/example/reviewms/projection/ReviewProjection.java
@@ -0,0 +1,9 @@
+package com.example.reviewms.projection;
+
+public interface ReviewProjection {
+    public String getTitle();
+
+    public String getDescription();
+
+    public double getRating();
+}
